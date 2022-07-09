bf291aeae3712fd75715cf19cf482bdbb14905f8
youkidkk <youkidkk80@gmail.com>
2022-07-09 19:52:18 +0900

    flake8 のエラーを修正

--- back/src/main.py
+++ back/src/main.py
@@ -30,7 +30,9 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "hashed_password": (
+            "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"
+        ),
         "disabled": False,
     }
 }
