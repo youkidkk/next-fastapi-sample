059d45d019d3a02b455da82ba4f40ad120fdd643
youkidkk <youkidkk80@gmail.com>
2022-07-18 15:32:46 +0900

    DBセッションの依存を修正

--- back/src/auth.py
+++ back/src/auth.py
@@ -80,7 +80,9 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-async def get_current_user(db: Session, token: str = Depends(oauth2_scheme)):
+async def get_current_user(
+    db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)
+):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
